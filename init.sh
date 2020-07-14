2dd20175f3e3b506aae66fa479f7448f8de448db
usayama <usa8ma@gmail.com>
2020-07-14 23:12:33 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,32 +10,37 @@ echo 'docker-composeのビルドが完了しました。'
 wait $!
 
 
-sleep 5
+sleep 3
 echo 'yarnのキャッシュをクリアします'
 docker-compose run --rm react yarn cache clean
 echo 'yarnのキャッシュをクリアしました'
 wait $!
 
 
-sleep 5
+sleep 3
 echo 'Reactアプリを作成します'
 docker-compose run --rm react npx create-react-app . --template typescript  --use-npm
 echo 'Reactアプリの作成が完了しました'
 wait $!
 
 
-sleep 5
+sleep 3
 echo 'パッケージをインストールします'
 docker-compose run --rm react npm install
 echo 'パッケージのインストールが完了しました'
 wait $!
 
 
-sleep 5
+sleep 3
 echo 'react-router-dom をインストールします'
 docker-compose run --rm react npm install react-router-dom @types/react-router-dom
 echo 'パッケージのインストールが完了しました'
 wait $!
 
+sleep 3
+echo 'tsconfig.json から jsx の行を探して、下に行を挿入します'
+gsed -i '/"jsx":/i \    "baseUrl": "src",' app/tsconfig.json
+echo '行の挿入が完了しました'
+wait $!
 
 echo '👍すべての処理が完了しました！'
